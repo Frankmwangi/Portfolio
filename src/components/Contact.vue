eaf9b41a3931718b9610b163586a02c46738ce26
Frank Mwangi <frankmwangi007@gmail.com>
2020-05-11 19:15:15 +0300

    contact page

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="contact-page">
-   <div class="row text-center">
+   <div class="row text-center ">
   <div class="col-md-4">
       <div class="card">
           <h1>Linked in</h1>
@@ -11,18 +11,29 @@
   <div class="col-md-4">
       <div class="card">
           <h1>Github</h1>
-           <a href="https://www.linkedin.com/in/frank-waweru-82664018b/" title="Linked-in">
+           <a href="https://github.com/Frankmwangi" title="Github">
            <i class="fab fa-github fa-7x"></i></a>
       
       </div></div>
   <div class="col-md-4">
       <div class="card">
           <h1>Email</h1>
-            <a href="https://www.linkedin.com/in/frank-waweru-82664018b/" title="Linked-in">
+            <a href="https://www.linkedin.com/in/frank-waweru-82664018b/" title="Email">
            <i class="far fa-envelope-square fa-7x"></i></a>
 
       </div></div>
 </div>
+<br>
+<br>
+    <div class="col-md-4 offset-4 call">
+        <div class="card">
+            <div class="card-body">
+            <h1>Call now</h1>
+            <p>0790971894<i class="fas fa-phone-square"></i></p>
+    </div>
+  </div>
+</div>
+
  
 </div>
       
@@ -54,5 +65,9 @@
   display: table;
   clear: both;
   
+}
+.call{
+    padding-right: 50px;
+    
 }
 </style>
